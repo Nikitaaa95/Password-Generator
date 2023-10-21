443da9137d8a7a90978cd8f658734cae7133c582
Nikita Thomas <nikitathomas@Nikitas-MBP.lan>
2023-10-21 17:10:59 -0400

    add requirement notes

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//Generate pop-ups showing up for the following
+///Length of characters
+////Between 8 and 128
+///Inclusion of lowercase
+///Inclusion of uppercase
+///Inclusion of numbers
+///Inclusion of special characters
+//Validate that at least one selection has been chosen
+//Generate password based on provided characteristics
+///List specific number of characters
+////For each character, cycle through the available options.
+
