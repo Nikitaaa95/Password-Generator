81a0caa0a46966d8ce136b7cd27d80870fda60ab
Nikita Thomas <nikitathomas@Nikitas-MBP.lan>
2024-02-22 22:06:29 -0500

    Debug and fix spacing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,54 @@
-// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
 function generatePassword() {
-    var length = parseInt(prompt("Enter the length of the password (8-128 characters):"));
+  var length = parseInt(prompt("Enter the length of the password (8-128 characters):"));
 
-    //Validation of at least 1 option
-    if (isNaN(length) || length < 8 || length > 128) {
-      alert("Please enter a valid number between 8 and 128.");
-      return "";
-    }
-    var lowercaselimit = window.prompt("Can the password include lowercase letters? (Y or N)");
-    lowercaselimit = lowercaselimit.toUpperCase();
-    var uppercaselimit = window.prompt("Can the password include uppercase letters? (Y or N)");
-    uppercaselimit = uppercaselimit.toUpperCase();
-    var numberlimit = window.prompt("Can the password include numbers? (Y or N)");
-    numberlimit = numberlimit.toUpperCase();
-    var spclchrlimit = window.prompt("Can the password include special characters? (Y or N)");
-    spclchrlimit = spclchrlimit.toUpperCase();
-
-    if (!(includeLowercase || includeUppercase || includeNumbers || includeSpecial)) {
-        alert("Please select at least one character type.");
-        return "";
-      }
-    // Array of options for computer to pick from
-    var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-    var numbercase = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    var spclchr = ["!", "@", "#", "$", "%", "^", "&", "*"];
-
-//include set of viable characters for password
-var allChars = "";
-    if (lowercaselimit) {
-      allChars += lowercase;
-    }
-    if (uppercaselimit) {
-      allChars += uppercase;
-    }
-    if (numberlimit) {
-      allChars += numbercase;
-    }
-    if (spclchrlimit) {
-      allChars += spclchr;
-    }
-
-
-//space to create password
-var password = "";
-    for (var i = 0; i < length; i++) {
-      password += allChars.charAt(Math.floor(Math.random() * allChars.length));
-    }
-  
-    return password;
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Please enter a valid number between 8 and 128.");
+    return "";
   }
 
-generateBtn.addEventListener("click", writePassword);   
+  var includeLowercase = prompt("Can the password include lowercase letters? (Y or N)").toUpperCase();
+  var includeUppercase = prompt("Can the password include uppercase letters? (Y or N)").toUpperCase();
+  var includeNumbers = prompt("Can the password include numbers? (Y or N)").toUpperCase();
+  var includeSpecial = prompt("Can the password include special characters? (Y or N)").toUpperCase();
 
-  
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
+  if (!(includeLowercase === 'Y' || includeUppercase === 'Y' || includeNumbers === 'Y' || includeSpecial === 'Y')) {
+    alert("Please select at least one character type.");
+    return "";
   }
-  
 
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var specialChars = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+  var allChars = "";
+  if (includeLowercase === 'Y') {
+    allChars += lowercase;
+  }
+  if (includeUppercase === 'Y') {
+    allChars += uppercase;
+  }
+  if (includeNumbers === 'Y') {
+    allChars += numbers;
+  }
+  if (includeSpecial === 'Y') {
+    allChars += specialChars;
+  }
+
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    password += allChars.charAt(Math.floor(Math.random() * allChars.length));
+  }
+
+  return password;
+}
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword);
