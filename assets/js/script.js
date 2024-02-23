8f72900e419313997d6e9bab11d311e52f65ac32
Nikita Thomas <nikitathomas@Nikitas-MBP.lan>
2024-02-22 20:51:28 -0500

    Removing some junk code and creating new array of only items to be included in password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Notes from Course - Add event listener to generate button
 
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-//   passwordText.value = password;
-// }
-// generateBtn.addEventListener("click", writePassword);
-
-
-//Button trigger
-generateBtn.addEventListener('click', function() {
-    var characterlimit = window.prompt("How many characters do you need?");
-
-    //Character Limit Prompt Actions
-if (characterlimit < 8 ) {
-    window.alert("Please choose a number between 8 and 128");
-    return;}
-else if (characterlimit > 128) {
-    window.alert("Please choose a number between 8 and 128");
-    return;}
-else {
     var lowercaselimit = window.prompt("Can the password include lowercase letters? (Y or N)");
     lowercaselimit = lowercaselimit.toUpperCase();
     var uppercaselimit = window.prompt("Can the password include uppercase letters? (Y or N)");
@@ -33,16 +11,26 @@ else {
     var spclchrlimit = window.prompt("Can the password include special characters? (Y or N)");
     spclchrlimit = spclchrlimit.toUpperCase();}
 
-//Pulling Random value
-// var index = Math.floor(Math.random() * characterlimit.length);
-// var pswdchr = characterlimit[index];
-// console.log(pswdchr)
-
     // Array of options for computer to pick from
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbercase = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var spclchr = ["!", "@", "#", "$", "%", "^", "&", "*"];
+
+//include set of viable characters for password
+var allChars = "";
+    if (lowercaselimit) {
+      allChars += lowercase;
+    }
+    if (uppercaselimit) {
+      allChars += uppercase;
+    }
+    if (numberlimit) {
+      allChars += numbercase;
+    }
+    if (spclchrlimit) {
+      allChars += spclchr;
+    }
 
 
 //Validation of at least 1 option
@@ -97,3 +85,16 @@ password.appendChild(passworddisplayEl)
 //Possible window alert option
 //window.alert = passwordfinal
 })
+
+//Button trigger
+generateBtn.addEventListener('click', function() {
+    var characterlimit = window.prompt("How many characters do you need?");
+
+    //Character Limit Prompt Actions
+if (characterlimit < 8 ) {
+    window.alert("Please choose a number between 8 and 128");
+    return;}
+else if (characterlimit > 128) {
+    window.alert("Please choose a number between 8 and 128");
+    return;}
+else {
